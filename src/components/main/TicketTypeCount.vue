1b2fbe1bff86f6f6439b3660cc77fa961d7b8d7d
Deji Martins <dejimartins99@gmail.com>
2024-07-15 02:07:12 +0100

    testing mobile qr structure

---
+++ src/components/main/TicketTypeCount.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="border border-2 border-[#C0C0C0] my-4 p-5 rounded-[22px]">
+        <div class="flex justify-between itens-center">
+            <p>{{ ticketType }}</p>
+            <div :class="`rounded-full bg-${color}-500 w-6 h-6 flex items-center justify-center`"></div>
+        </div>
+        <p class="font-bold text-3xl">{{ totalTicketsValidated }}/{{ totalTicketsSold }}</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    ticketType: string
+    totalTicketsValidated: number
+    totalTicketsSold: number
+    color: string
+}>()
+</script>
