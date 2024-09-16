d6dab268f47c973e945c7dea2a87c847c68e809f
Deji Martins <dejimartins99@gmail.com>
2024-09-16 10:44:14 +0100

    api utils: removed get-event and get-all-events from apis

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,8 @@
 const Api = () => {
   const BASE_URL = 'https://rave.onrender.com'
   const BASE_URL_PAYMENT = 'https://partybank-29cd4a45490f.herokuapp.com'
-  // const GET_ALL_EVENTS = `${BASE_URL}/api/v1/events`
+
   const SUBSCRIBE = `${BASE_URL_PAYMENT}/api/v1/subscribe`
-  const GET_EVENT = `${BASE_URL_PAYMENT}/api/v1/events`
   const PAY = `${BASE_URL_PAYMENT}/api/v1/payment`
   const SEARCH_TICKET = `${BASE_URL_PAYMENT}/api/v1/search`
   const VALIDATE_TICKET = `${BASE_URL_PAYMENT}/api/v1/validate`
@@ -14,9 +13,7 @@ const Api = () => {
   const GET_EVENT_BY_REFERENCE = `${BASE_URL}/api/v1/event/reference`
 
   return {
-    // GET_ALL_EVENTS,
     SUBSCRIBE,
-    GET_EVENT,
     PAY, SEARCH_TICKET, VALIDATE_TICKET,
     GET_PURCHASED_TICKETS,
     DISCOVER_EVENTS,
