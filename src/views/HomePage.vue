4e0c603f3672951cedad2869a9ff6b5083e87973
Deji Martins <dejimartins99@gmail.com>
2024-09-22 19:09:57 +0100

    landing: hero-section change

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,9 +1,14 @@
 <template>
     <main>
         <Header />
+
+        <div class="px-2 bg-[#FFF3F5]">
+            <HeroSection />
+        </div>
     </main>
 </template>
 
 <script setup lang="ts">
-import Header from '@/components/Header.vue';
+import Header from '@/components/home-page/Header.vue';
+import HeroSection from '@/components/home-page/HeroSection.vue';
 </script>
