74ef6a45e42db01d5cb59bc55f467763a6bc290e
Deji Martins <dejimartins99@gmail.com>
2024-09-26 03:15:05 +0100

    back button navigate working

--- src/components/buttons/BackButton.vue
+++ src/components/buttons/BackButton.vue
@@ -1,17 +1,30 @@
 <template>
-     <RouterLink
-        class="border cursor-pointer bg-[#F3F5F5] px-[10px] py-[5px] rounded-[15px]"
-        :to="href"
+    <button 
+      @click="navigateBack" 
+      class="border cursor-pointer bg-[#F3F5F5] px-[10px] py-[5px] rounded-[15px]"
     >
-        <ArrowLongLeftIcon class="w-[20px] h-[30px] stroke-2"/>
-     </RouterLink>
+      <ArrowLongLeftIcon class="w-[20px] h-[30px] stroke-2"/>
+    </button>
 </template>
-
+  
 <script setup lang="ts">
 import { ArrowLongLeftIcon } from '@heroicons/vue/24/solid';
-import { RouterLink } from 'vue-router';
+import { useRouter } from 'vue-router';
 
-defineProps<{
+const props = defineProps<{
     href: string
-}>()
+}>();
+
+const router = useRouter();
+
+const navigateBack = () => {
+    const previousPage = localStorage.getItem('previousPage');
+
+    if (previousPage) {
+        router.push(previousPage); 
+        localStorage.removeItem('previousPage');
+    } else {
+        router.push(props.href);
+    }
+};
 </script>
