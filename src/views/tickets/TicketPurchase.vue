e05465363471ba72a1b35671b56fe559b0fee66f
Deji Martins <dejimartins99@gmail.com>
2024-09-28 02:58:57 +0100

    ticket summary done

--- src/views/tickets/TicketPurchase.vue
+++ src/views/tickets/TicketPurchase.vue
@@ -4,12 +4,12 @@
 
         <div class="flex-grow flex flex-col bg-[var(--pb-c-white)] overflow-y-auto mt-24 md:mx-6 rounded-t-[30px]">
             <div class="flex items-center gap-7 py-3 border-0 border-b-[3px] px-6 md:px-20 border-[var(--pb-c-soft-grey)]">
-                <BackButton href="/" />
+                <BackButton href="/discover" />
                 <p class="text-[23px] md:text-[30px] font-[700]">Ticket Purchase</p>
             </div>
-            
+
             <div class="flex-grow px-6 md:px-20 overflow-hidden">
-                <TicketSummmary />
+                <TicketSummary />
             </div>
         </div>
 
@@ -19,5 +19,5 @@
 <script setup lang="ts">
 import BackButton from '@/components/buttons/BackButton.vue';
 import Header from '@/components/ui/HeaderBar.vue';
-import TicketSummmary from '@/components/events/tickets/TicketSummmary.vue';
+import TicketSummary from '@/components/events/tickets/TicketSummary.vue';
 </script>
