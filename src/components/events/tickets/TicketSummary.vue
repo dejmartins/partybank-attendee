4a46825a062da42723ee6a2381ef2020b4855be6
Deji Martins <dejimartins99@gmail.com>
2024-10-01 00:19:40 +0100

    fixes: redesign ticket purchase page

--- src/components/events/tickets/TicketSummary.vue
+++ src/components/events/tickets/TicketSummary.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col md:flex-row w-full h-[calc(100vh-210px)] overflow-y-auto md:overflow-y-hidden rounded-[20px] bg-[#F9F7F7] p-6 md:p-10 mt-4 border border-[#DDE0E3] custom-scrollbar">
-    <div class="w-full md:w-1/2 px-0 md:px-2 ">
+    <div class="w-full md:w-1/2 px-0 md:px-2">
       <p class="font-[700] text-[24px] md:text-[30px] mb-2 md:mb-0">My Info</p>
       <form class="form-container p-3 md:p-6 rounded-[20px]">
         <div class="form-group">
@@ -56,27 +56,32 @@
     <div class="w-full md:w-1/2 px-0 md:px-2 md:overflow-y-auto h-[calc(100vh-290px)] custom-scrollbar">
       <p class="font-[700] text-[24px] md:text-[30px] mt-5 md:mt-0 mb-2 md:mb-0">Ticket Info</p>
       <div class="rounded-[20px] bg-[#FFFFFF] p-3 md:p-6">
-        <div class="w-full mb-4">
-          <img :src="eventStore.eventImage || '/defaultImage.png'" alt="Event Image" class="w-full h-[100px] md:h-[150px] object-cover rounded-[20px] border border-[#DDE0E3]" />
-        </div>
-        <div class="">
-          <div class="border-b pb-2">
-            <p class="font-[600] text-[24px] md:text-[27px]">{{ eventStore.eventName }}</p>
-            <p class="font-[400] text-[#B5BBC0] text-[16px] md:text-[20px]">{{ currentDate }}</p>
+        <!-- <div class="w-full mb-4">
+          <img :src="eventStore.eventImage || '/defaultImage.png'" alt="Event Image" class="w-full h-[50px] object-cover rounded-[20px] border border-[#DDE0E3]" />
+        </div> -->
+        <div>
+          <div class="flex items-stretch gap-3 border-b pb-2">
+            <div class="hidden md:flex w-full md:w-[20%] h-full">
+              <img :src="eventStore.eventImage || '/defaultImage.png'" alt="Event Image" class="w-full h-[100px] object-cover rounded-[10px] border border-[#DDE0E3]" />
+            </div>
+            <div class="flex flex-col justify-center">
+              <p class="font-[600] text-[24px] md:text-[27px]">{{ eventStore.eventName }}</p>
+              <p class="font-[400] text-[#B5BBC0] text-[16px] md:text-[20px]">{{ currentDate }}</p>
+            </div>
           </div>
 
           <div class="border-b pb-2">
-            <div class="flex justify-between text-[#B5BBC0] font-[400] text-[16px] md:text-[20px] my-3">
+            <div class="flex justify-between text-[#B5BBC0] font-[400] text-[16px] md:text-[20px] my-3 gap-4">
               <p>Total Ticket</p>
               <p>{{ eventStore.ticketQuantity }} x {{ eventStore.ticketType }}</p>
             </div>
-            <div class="flex justify-between font-[400] text-[16px] md:text-[20px]">
+            <div class="flex justify-between font-[400] text-[16px] md:text-[20px] gap-4">
               <p class="text-[#B5BBC0]">Amount</p>
               <p><span class="text-[12px]">NGN</span> {{ formatAmountWithCommas(eventStore.ticketAmount) }}</p>
             </div>
           </div>
 
-          <div class="flex justify-between mt-4 text-[20px] md:text-[22px] md:text-[28px]">
+          <div class="flex justify-between mt-4 text-[20px] md:text-[22px] md:text-[28px] gap-4">
             <p class="font-[700]">Total Payment</p>
             <p class="font-[700]"><span class="text-[12px]">NGN</span> {{ formatAmountWithCommas(total) }}</p>
           </div>
@@ -95,7 +100,7 @@
         action="Proceed to Payment"
         :disabled="disabled"
         :loading="disabled"
-        additional-classes="bg-[var(--pb-c-red)] border-[var(--pb-c-red)] text-[var(--pb-c-white)] w-full my-5"
+        additional-classes="bg-[var(--pb-c-red)] border-[var(--pb-c-red)] text-[var(--pb-c-white)] w-full my-5 mb-10"
         text-style="text-[18px] font-[700]"
         additional-loader-classes="border-4 border-t-[var(--pb-c-red)]"
         @click="handleProceedToPayment"
