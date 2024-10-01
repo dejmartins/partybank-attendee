59407d640abbde277d604a5e5d0560ddf2898266
Deji Martins <dejimartins99@gmail.com>
2024-10-01 16:32:07 +0100

    fixes: layout corrected

--- src/views/events/DiscoverEvents.vue
+++ src/views/events/DiscoverEvents.vue
@@ -1,11 +1,11 @@
 <template>
-  <main class="fixed w-full flex flex-col h-screen border-[var(--pb-c-soft-grey)] rounded-t-[40px]">
-    <div class="flex items-center gap-7 py-3 border-0 border-b-[3px] px-6 md:px-20 border-[var(--pb-c-soft-grey)]">
+  <main class="w-full flex flex-col border-[var(--pb-c-soft-grey)] rounded-t-[40px]">
+    <div class="flex items-center gap-7 py-3 border-0 border-b-[3px] px-6 lg:px-20 border-[var(--pb-c-soft-grey)]">
       <BackButton href="/" />
       <p class="text-[23px] md:text-[30px] font-[700]">Discover Events</p>
     </div>
 
-    <div class="flex-grow px-6 md:px-20 overflow-hidden">
+    <div class="px-6 lg:px-20 overflow-hidden">
       <StateDropDown 
         :states="states"
         v-model="selectedState"
